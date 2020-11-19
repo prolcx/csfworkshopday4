b447e86e9493b437fc89dcac49a555aa532f0460
DavidLee <tisdalearmstrong@gmail.com>
2020-11-19 15:11:41 +0800

    'firebase'

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,79 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder, Validators, FormArray } from '@angular/forms';
+import { v4 as uuidv4 } from 'uuid';
+import { Todo } from './todo';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'ToDo';
+export class AppComponent implements OnInit{
+  title = 'todoApp';
+  form: FormGroup;
+  tomorrow = new Date();
+  todoValues = [];
+
+  constructor(private fb: FormBuilder){
+    this.tomorrow.setDate(this.tomorrow.getDate() +1);
+  }
+
+  ngOnInit(){
+    this.form = this.fb.group({
+      task: this.fb.control('', [Validators.required]),
+      priority: this.fb.control('', [Validators.required]),
+      dueDate: this.fb.control('', [Validators.required]),
+      todos: new FormArray([]),
+    });
+  }
+
+  getPriorityErrorMessage(){
+    if (this.form.get('priority').hasError('required')) {
+      return 'You must enter a value';
+    }
+  }
+
+  addTodo(){
+    console.log("add todo");
+    console.log(this.form.value.task);
+    console.log(this.form.value.dueDate);
+    console.log(this.form.value.priority);
+    const todosWithChkbox : FormArray = this.form.get('todos') as FormArray;
+    let taskId = uuidv4();
+    console.log(taskId);
+    let singleTodo = new Todo(
+      this.form.value.task,
+      this.form.value.priority,
+      this.form.value.dueDate,
+      taskId
+    );
+    this.todoValues.push(singleTodo);
+    const todoGroup = this.fb.group({
+      task: this.fb.control(false)
+    });
+    todosWithChkbox.push(todoGroup);
+    this.form.get('task').reset();
+    this.form.get('priority').reset();
+    this.form.get('dueDate').reset();
+    // get from localstorage then update the todo array??
+    localStorage.setItem(taskId, JSON.stringify(singleTodo));
+  }
+  
+  updateStatus(index){
+    this.todoValues[index].status=true;
+  }
+
+  onDelete(index, taskId){
+    const todosWithChkbox : FormArray = this.form.get('todos') as FormArray;
+    todosWithChkbox.removeAt(index);
+    let deleteRecordFound = this.todoValues.indexOf(index,0);
+    if(index > -1){
+      this.todoValues.splice(deleteRecordFound, 1);
+    }
+    localStorage.removeItem(taskId);
+  }
+
+  onEdit(index, taskId){
+
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,32 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { ReactiveFormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
-import { RegistrationComponent } from './components/registration.component';
+import {FlexLayoutModule } from '@angular/flex-layout';
+import {FormsModule, ReactiveFormsModule} from '@angular/forms';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MAT_MOMENT_DATE_FORMATS, MomentDateAdapter, MAT_MOMENT_DATE_ADAPTER_OPTIONS} from '@angular/material-moment-adapter';
+
+import { DateAdapter, MAT_DATE_FORMATS, MAT_DATE_LOCALE} from '@angular/material/core';
+import { MaterialModule } from './material.module';
 
 @NgModule({
   declarations: [
     AppComponent,
-    RegistrationComponent
   ],
   imports: [
     BrowserModule,
-    ReactiveFormsModule
+    ReactiveFormsModule,
+    FormsModule,
+    FlexLayoutModule,
+    BrowserAnimationsModule,
+    MaterialModule
   ],
-  providers: [],
+  providers: [
+    { provide: MAT_DATE_LOCALE, useValue: 'en-SG'},
+    { provide: DateAdapter, useClass: MomentDateAdapter, deps: [MAT_DATE_LOCALE, MAT_MOMENT_DATE_ADAPTER_OPTIONS]},
+    { provide: MAT_DATE_FORMATS, useValue: MAT_MOMENT_DATE_FORMATS}
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
